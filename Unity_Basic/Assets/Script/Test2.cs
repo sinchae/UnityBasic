78821bfcbe299b3ddd2ed8c60c912e94bc11cb57
sinchae <cogus6509@naver.com>
2025-06-05 17:29:28 +0900

    [add] JokeBearButton script
    농담곰 버튼을 스크립트와 연동함

---
+++ Unity_Basic/Assets/Script/Test2.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+{
+    public TextMeshProUGUI Txt_Text;
+    public string NextText;
+
+    public void OnClickButton()
+    {
+        Txt_Text.text ="³ª´Â Çï½º ³ó´ã°õ.";
+    }
+ 
+}
